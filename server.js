c34ae59cde802d48376e237e2b5b601fc44a665f
Dan Harten <big.styx08@gmail.com>
2018-03-12 15:46:35 -0600

    initial heroku commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const port = process.env.PORT || 5150
 
@@ -10,7 +11,7 @@ app.use(bodyParser.json());
 app.use(morgan("dev"));
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/concrete-testing",
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/concrete-testing",
   {useMongoClient: true},
   err => {
     if (err) throw err;
@@ -19,6 +20,11 @@ mongoose.connect("mongodb://localhost/concrete-testing",
 );
 
 app.use("/concrete", require("./routes/jobRoutes"));
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);

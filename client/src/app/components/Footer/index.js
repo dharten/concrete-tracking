602dfbea8ecffa33cd840f3d166a01f1eecfb53e
Dan Harten <big.styx08@gmail.com>
2018-03-13 10:11:26 -0600

    modify footer

--- client/src/app/components/Footer/index.js
+++ client/src/app/components/Footer/index.js
@@ -4,7 +4,7 @@ import "./footer.css";
 export default function Footer(props) {
   return(
     <div className="footer">
-      <p>&copy; <span>Patent Pending</span></p>
+      <p>&copy; <span>2017</span></p>
 
     </div>
   )
